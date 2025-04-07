5108cfe87afc8c394fb390be62cfe97993d81d47
prime8489 <botsend0@gmail.com>
2025-04-07 07:58:20 +0530

    Create terabox.js

---
+++ api/terabox.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "No URL provided" });
+  }
+
+  res.status(200).json({
+    message: "TeraBox API working!",
+    your_url: url
+  });
+}
